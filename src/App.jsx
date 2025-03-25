e6db67e14ce7ff0476a7d05e75ebd6a383092f9c
janada jayathilaka <lakvindujayathilaka@gmail.com>
2025-03-25 22:50:47 +0530

    bookings page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from "./components/home/HomePage";
 import FooterComponent from "./components/common/Footer";
 import React from "react";
 import "./index.css";
+import AllRoomsPage from "./components/booking_rooms/AllRoomsPage";
+import FindBookingPage from "./components/booking_rooms/FIndBookingPage";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/rooms" element={<AllRoomsPage />} />
+        <Route path="/find-booking" element={<FindBookingPage />} />
       </Routes>
       <FooterComponent />
     </BrowserRouter>
